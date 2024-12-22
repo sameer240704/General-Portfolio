e0058ce5b1d56bd2f9a25d8717b1e86991d19c69
sameer240704 <sameergupta79711@gmail.com>
2024-12-22 12:48:58 +0530

    Initial setup

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-galaxyImg bg-repeat bg-cover bg-opacity-20 text-white">
+      <h1 className="text-3xl font-jedi">Hello World</h1>
+      <h2 className="text-2xl font-outline">Hey there</h2>
+    </div>
+  );
+}
